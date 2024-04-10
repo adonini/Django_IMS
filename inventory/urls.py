33fe2647c99ec2f63c0a97a86b2d2098228a7b1d
Alice Donini <alixd90@gmail.com>
2024-04-10 09:10:38 +0200

    update urls

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Index, SignUpView, Stock, AddItem
 from django.contrib.auth import views as auth_views
 from . import views
 
+
 urlpatterns = [
-    path('', Index.as_view(), name='index'),  # point the root path to index view
-    path('stock/', Stock.as_view(), name='stock'),
-    path('add-item/', AddItem.as_view(), name='add-item'),
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path('', views.Index.as_view(), name='index'),  # point the root path to index view
+    path('stock/', views.Stock.as_view(), name='stock'),
+    #path('add-item/', views.AddItem.as_view(), name='add-item'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
     path('logout/', views.logout_user, name='logout'),  # auth_views.LogoutView.as_view(template_name='inventory/logout.html'), name='logout'),
+    path('items/<int:pk>/', views.Item_record.as_view(), name='item_record'),
+    path('stock/<int:pk>/', views.Stock_record.as_view(), name='stock_record'),
+    path('items/', views.Item_list.as_view(), name='item_list'),
 ]
